ab68e47f1776304624a5ae011110e39c362e549a
Raymond Wang <128938880+Rayray1237@users.noreply.github.com>
2024-09-21 22:53:46 -0500

    Add files via upload

---
+++ pa1c/README.md
@@ -0,0 +1,48 @@
+## Due Dates:
+
+Early turn-in: February 16, 11:59 PM
+Hard turn-in: February 19, 11:59 PM
+
+## Getting started
+
+For this assignment, you'll need Node again.
+
+## Assignments
+
+You will implement a lazy-evaluation system in JavaScript (lazy.js).  
+As before, we have removed the solutions and
+included an opening and closing comment `/** <FILL-IN> **/` where you are
+expected to fill in your answers.  You may not add code anywhere outside these
+comment blocks.
+
+### Testing your solution
+
+You can run the code by calling Node:
+
+For example:
+
+```bash
+node lazy.js
+```
+
+#### No-nos
+
+In all cases we are exporting the answers (e.g., `exports.fold_left = ...`),
+please do not remove any of this code. If we can't test your code, you lose
+points.
+
+#### Grading
+
+We have included the points-worth of each question in the comments.
+The maximum score is 35 points.
+
+#### Submission
+
+You must submit the assignment via gradescope. 
+Submit your assignment by uploading the following files only:
+
+- lazy.js
+
+Unlike our first assignments, the autograder will not look for your files or
+otherwise fix your bugs. You may only upload these files and you must ensure
+they can be loaded.
---
+++ pa7/pa7/rust-1/src/rpn.rs
@@ -0,0 +1,138 @@
+/*
+ * Reverse Polish Notation: rpn.rs
+ * See `rpn.md` for the overview.
+ */
+
+use std::io;
+use rand::{thread_rng, Rng};
+// Stacks will work with Items, which either either integers or booleans
+#[derive(Eq, PartialEq, Ord, PartialOrd, Debug)]
+pub enum Item {
+    Int(i32),
+    Bool(bool),
+}
+
+impl Item {
+    pub fn int(self) -> Result<i32> {
+        match self {
+            Item::Int(t) => Ok(t),
+            _ => Err(Error::Type),
+        }
+    }
+
+    pub fn bool(self) -> Result<bool> {
+        match self {
+            Item::Bool(t) => Ok(t),
+            _ => Err(Error::Type),
+        }
+    }
+}
+
+// List of possible errors
+#[derive(Debug)]
+pub enum Error {
+    Empty,         // Tried to pop empty stack
+    Extra,         // Stack ended with extra elements
+    Type,          // Type mismatch
+    Syntax,        // Syntax error, didn't recognize op
+    IO(io::Error), // Some kind of IO error
+    Quit,          // User quitting
+}
+
+// Base operations supported by calculator, see rpn.md
+#[derive(Debug)]
+pub enum Op {
+    Add,
+    Eq,
+    Neg,
+    Swap,
+    Rand,
+    Cond,
+    Quit,
+}
+
+// We'll define a result type for our calculator: either a valid value, or a calculator Error
+pub type Result<T> = std::result::Result<T, Error>;
+
+// Define a type for Stacks
+#[derive(Debug)]
+pub struct Stack(Vec<Item>);
+
+// Implement the following functions on Stacks
+impl Stack {
+    // Make a new Stack
+    pub fn new() -> Self {
+        Self(Vec::new())
+    }
+
+    // Check if a Stack is empty
+    pub fn empty(&self) -> bool {
+        self.0.is_empty()
+    }
+
+    // Push an item onto a stack (should never error)
+    pub fn push(&mut self, item: Item) -> Result<()> {
+        self.0.push(item);
+        Ok(())
+    }
+
+    // Pop an item off the Stack; may result in Empty error
+    pub fn pop(&mut self) -> Result<Item> {
+        match self.0.pop() {
+            Some(val) => Ok(val),
+            None => Err(Error::Empty),
+        }
+    }
+
+    /*
+     * Main evaluation function: apply an operation to a Stack
+     *
+     * Complete each of the cases. 
+     *
+     * Hint: You'll probably want to use the "question-mark" syntax quite a bit; see `rpn.md`.
+     */
+    pub fn eval(&mut self, op: Op) -> Result<()> {
+        match op {
+            Op::Add => {
+                let b = self.pop()?;
+                let a = self.pop()?;
+                let result = Item::Int(a.int()? + b.int()?);
+                self.push(result)
+            }
+            Op::Eq => {
+                let b = self.pop()?;
+                let a = self.pop()?;
+                let result = match (a, b) {
+                    (Item::Int(x), Item::Int(y)) => Item::Bool(x == y),
+                    (Item::Bool(x), Item::Bool(y)) => Item::Bool(x == y),
+                    _ => return Err(Error::Type),
+                };
+                self.push(result)
+            }
+            Op::Neg => {
+                let a = self.pop()?.bool()?;
+                let result = Item::Bool(!a);
+                self.push(result)
+            }
+            Op::Swap => {
+                let b = self.pop()?;
+                let a = self.pop()?;
+                self.push(b)?;
+                self.push(a)
+            }
+            Op::Rand => {
+                let a = self.pop()?.int()?;
+                let mut rng = thread_rng();
+                let result = Item::Int(rng.gen_range(0, a));
+                self.push(result)
+            }
+            Op::Cond => {
+                let b = self.pop()?;
+                let a = self.pop()?;
+                let cond = self.pop()?.bool()?;
+                self.push(if cond { a } else { b })
+            }
+            Op::Quit => Err(Error::Quit),
+        }
+    }
+}
